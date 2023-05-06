734c00a064a1bc92f4d52dd7c8352d8987c7f919
Luca <lucadesimone0601@gmail.com>
2023-05-05 21:18:46 -0300

    login fix

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -8,13 +8,12 @@ import {
 }
 from 'mdb-react-ui-kit';
 import "./login1.scss";
+import Navbar from "../../components/navbar/Navbar";
 
 function App() {
   return (
-    <MDBContainer className="my-5 gradient-form">
-
+    <MDBContainer className="my-6 gradient-form"  color='black'>
       <MDBRow>
-
         <MDBCol col='6' className="mb-5">
           <div className="d-flex flex-column ms-5">
 
@@ -26,10 +25,8 @@ function App() {
 
             <p>Please login to your account</p>
 
-
             <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email'/>
             <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password'/>
-
 
             <div className="text-center pt-1 mb-5 pb-1">
               <MDBBtn className="mb-4 w-100 gradient-custom-2">Sign in</MDBBtn>
@@ -47,7 +44,7 @@ function App() {
 
         </MDBCol>
 
-        <MDBCol col='6' className="mb-5">
+        <MDBCol col='6' className="mb-5"  >
           <div className="d-flex flex-column  justify-content-center gradient-custom-2 h-100 mb-4">
 
             <div className="text-white px-3 py-4 p-md-5 mx-md-4">
